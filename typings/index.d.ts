a499b8bdbbd3827012b501f4bc90cddf4977a3d7
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2016-08-11 17:48:46 +0200

    Typings

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/angular-protractor/index.d.ts" />
+/// <reference path="globals/es6-shim/index.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/selenium-webdriver/index.d.ts" />
