16a10f88ddf04de1dcbd83c8a87a43956b156b9d
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2016-08-11 17:38:34 +0200

    remove typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/angular-protractor/index.d.ts" />
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/selenium-webdriver/index.d.ts" />
