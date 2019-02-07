7f3704810e8964eef96f5a26eac5715ddfcbede0
xxxtonixxx <tonivj5@gmail.com>
2019-02-07 14:10:45 +0100

    fix: missing token export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 export { InlineEditorComponent } from './ngx-inline-editor.component';
 export { InlineEditorModule } from './ngx-inline-editor.module';
-export {
-  INLINE_EDITOR_TEMPLATE_CONFIG,
-  INLINE_EDITOR_INPUTS,
-  InlineEditorEvents,
-} from './common/index';
+export * from './common/index';
 export { InlineEditorService } from './ngx-inline-editor.service';
