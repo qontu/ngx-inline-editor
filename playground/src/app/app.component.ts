ed8c0a64c481408f5d8ec0380aaa1f2f5231179d
xxxtonixxx <tonivj5@gmail.com>
2019-03-11 16:16:50 +0100

    docs(playground): use api

--- playground/src/app/app.component.ts
+++ playground/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { InlineEditorService } from '@qontu/ngx-inline-editor';
 import { InputTextComponent } from 'src/inputs/src/text/input-text.component';
+import { InputPasswordComponent } from 'src/inputs/src/password/input-password.component';
 
 @Component({
   selector: 'app-root',
@@ -20,13 +21,26 @@ export class AppComponent {
   ]);
 
   toggle() {
-    // this.reactiveTest.disabled
-    //   ? this.reactiveTest.enable()
-    //   : this.reactiveTest.disable();
-    // this.inlineEditorService.getInputsOfType()
-    const [input] = this.inlineEditorService.getInputsOfType(
-      InputTextComponent,
-    );
-    input.toggle();
+    this.reactiveTest.disabled
+      ? this.reactiveTest.enable()
+      : this.reactiveTest.disable();
+
+    const [input] = this.inlineEditorService
+      .getInputsOfType(InputPasswordComponent)
+      .map(input => input.getApi());
+
+    input.toggle({ focus: true });
+  }
+
+  updateValue() {
+    const [input] = this.inlineEditorService
+      .getInputsOfType(InputPasswordComponent)
+      .map(input => input.getApi());
+
+    // input.setState({ value: input.getState().value + ' hi' });
+
+    input.getState().isValid ? input.markAsInvalid() : input.markAsValid();
+
+    // input.save(input.getState().value + ' hi');
   }
 }
